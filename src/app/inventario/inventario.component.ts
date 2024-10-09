8ca8d469fd8567e9810536bbfc23c258a17b8c91
joelocano <joelangelocano@gmail.com>
2024-10-08 20:41:37 -0500

    init

---
+++ src/app/inventario/inventario.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { NavbarComponent } from "../navbar/navbar.component";
+
+@Component({
+  selector: 'app-inventario',
+  standalone: true,
+  imports: [NavbarComponent],
+  templateUrl: './inventario.component.html',
+  styleUrl: './inventario.component.css'
+})
+export class InventarioComponent {
+
+}
